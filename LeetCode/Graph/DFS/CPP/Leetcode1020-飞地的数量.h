83441a52df7fa0693d18e603b5c55da278c9c6ca
simonhancrew <597494370@qq.com>
2022-02-12 10:34:00 +0800

    flood fill

---
+++ LeetCode/Graph/DFS/CPP/Leetcode1020-飞地的数量.h
@@ -0,0 +1,41 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    const int dx[4] = {0,-1,0,1},dy[4] = {-1,0,1,0};
+    int n,m;
+    int dfs(int x,int y,vector<vector<int>>& grid,bool &tag) 
+    {
+        int sum = 1;
+        if(!tag && (x == 0 || y == 0 || x == n - 1 || y == m - 1)) tag = true;
+        grid[x][y] = 0;
+        for(int i = 0;i < 4;i++) 
+        {
+            int nx = x + dx[i],ny = y + dy[i];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(!grid[nx][ny]) continue;
+            sum += dfs(nx,ny,grid,tag);
+        }
+        return sum;
+    }
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        int ans = 0;
+        n = grid.size(),m = grid[0].size();
+        for(int i = 0;i < n;i++) 
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(grid[i][j] == 1) 
+                {
+                    bool tag = false;
+                    int sum = dfs(i,j,grid,tag);
+                    if(!tag) ans += sum;
+                }  
+            }
+        }
+        return ans;
+    }
+};
---
+++ LeetCode/Graph/DFS/Python/Leetcode1020-飞地的数量.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        n,m,ans = len(grid),len(grid[0]),0
+        def dfs(x,y):
+            nonlocal tag
+            dx,dy = [0,-1,0,1],[-1,0,1,0]
+            ss = 1
+            grid[x][y] = 0
+            if not tag and (x == 0 or x == n - 1 or y == 0 or y == m - 1): tag = True
+            for i in range(4):
+                nx,ny = x + dx[i],y + dy[i]
+                if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
+                if grid[nx][ny] == 0: continue
+                ss += dfs(nx,ny)
+            return ss
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    tag = False
+                    ss = dfs(i,j)
+                    if not tag: ans += ss
+        return ans
