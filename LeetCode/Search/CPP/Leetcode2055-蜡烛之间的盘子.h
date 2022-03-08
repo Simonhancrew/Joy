dccef966f2d3ab15a33cab2249f1a4832e52ec28
simonhancrew <597494370@qq.com>
2022-03-08 12:17:38 +0800

    presum + binarysearch

---
+++ LeetCode/Search/CPP/Leetcode2055-蜡烛之间的盘子.h
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> platesBetweenCandles(string s, vector<vector<int>> &queries)
+    {
+        int n = s.size();
+        vector<int> pre(n + 1), idx, ans;
+        for (int i = 1; i <= n; i++)
+        {
+            if (s[i - 1] == '*')
+            {
+                pre[i] = pre[i - 1] + 1;
+            }
+            else
+            {
+                pre[i] = pre[i - 1];
+                idx.push_back(i - 1);
+            }
+        }
+        for (auto q : queries)
+        {
+            int l = q[0], r = q[1];
+            int ll = lower_bound(idx.begin(), idx.end(), l) - idx.begin();
+            int rr = upper_bound(idx.begin(), idx.end(), r) - idx.begin() - 1;
+            if (rr < ll)
+                ans.push_back(0);
+            else
+                ans.push_back(pre[idx[rr]] - pre[idx[ll]]);
+        }
+        return ans;
+    }
+};
---
+++ LeetCode/Search/python/Leetcode2055-蜡烛之间的盘子.py
@@ -0,0 +1,28 @@
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        ans,pre,idx = [],[0] * (n + 1), []
+        for i in range(1,n + 1):
+            if s[i - 1] == '*':
+                pre[i] = pre[i - 1] + 1
+            else:
+                pre[i] = pre[i - 1]
+                idx.append(i - 1)
+        if not idx: return [0] * len(queries)
+        for a,b in queries:
+            l,r = 0,len(idx) - 1
+            while l < r:
+                mid = l + r >> 1
+                if idx[mid] >= a: r = mid
+                else: l = mid + 1
+            ll = l
+            l,r = 0,len(idx) - 1
+            while l < r:
+                mid = l + r + 1 >> 1
+                if idx[mid] <= b: l = mid
+                else: r = mid - 1
+            rr = l
+            if rr < ll: ans.append(0)
+            else: ans.append(pre[idx[rr]] - pre[idx[ll]])
+
+        return ans
