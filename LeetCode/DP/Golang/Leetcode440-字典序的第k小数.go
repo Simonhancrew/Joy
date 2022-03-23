b59ea22f46721eca69c7268e0af890b06c90a26e
simonhancrew <597494370@qq.com>
2022-03-23 10:34:26 +0800

    go version

--- LeetCode/DP/Golang/Leetcode440-字典序的第k小数.go
+++ LeetCode/DP/Golang/Leetcode440-字典序的第k小数.go
@@ -1,28 +1,31 @@
 package sbgo
 
-var findKthNumber = function(n, k) {
-    let pre = 1;
-    let f = function(pre,n) {
-        let a = String(n),b = String(pre);
-        const d = a.length - b.length;
-        let res = 0,p = 1;
-        for(let i = 0;i < d;i++) {
-            res += p;
-            p *= 10;
+func findKthNumber(n int, k int) int {
+    pre := 1
+    f := func(pre,n int) int {
+        a,b := strconv.Itoa(n),strconv.Itoa(pre)
+        d := len(a) - len(b)
+        res,p := 0,1
+        for i := 0;i < d;i++ {
+            res += p
+            p *= 10
         }
-        if(a.substr(0,b.length) == b) res += n - pre * p + 1;
-        else if(a.substr(0,b.length) > b) res += p;
-        return res;
+        if a[0:len(b)] == b {
+            res += n - pre * p + 1
+        }else if a[0:len(b)] > b {
+            res += p
+        }
+        return res
     }
-    while(k > 1) {
-        const cnt = f(pre,n);
-        if(k > cnt) {
-            k -= cnt;
-            pre++;
+    for k > 1 {
+        cnt := f(pre,n)
+        if k > cnt {
+            k -= cnt
+            pre++
         }else{
-            pre *= 10;
-            k--;
+            k--
+            pre *= 10
         }
     }
-    return pre;
-};
+    return pre
+}
