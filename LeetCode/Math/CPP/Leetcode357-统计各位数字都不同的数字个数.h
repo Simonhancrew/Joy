7dfccfde02e1f2c663c3a0fd40b7a706cd03be7b
simonhancrew <597494370@qq.com>
2022-04-11 09:54:27 +0800

    add dfs solution

--- LeetCode/Math/CPP/Leetcode357-统计各位数字都不同的数字个数.h
+++ LeetCode/Math/CPP/Leetcode357-统计各位数字都不同的数字个数.h
@@ -16,3 +16,26 @@ public:
         return res;
     }
 };
+
+/*
+class Solution {
+public:
+    int res = 0;
+    void dfs(int u,int st,int n) {
+        if(u == n) return;
+        for(int i = 0;i <= 9;i++) {
+            if(!((st >> i) & 1)) {
+                if(u == 0 && i == 0) continue;
+                st |= 1 << i;
+                res++;
+                dfs(u + 1,st,n);
+                st -= 1 << i;
+            }
+        }
+    }
+    int countNumbersWithUniqueDigits(int n) {
+        dfs(0,0,n);
+        return res + 1;
+    }
+};
+*/
