2c17a5c61ea554e06da292272a30a51950bc61b6
simonhancrew <597494370@qq.com>
2022-05-24 10:52:44 +0800

    dfs

---
+++ LeetCode/Graph/DFS/Rust/Leetcode965-univalued-binary-tree.rs
@@ -0,0 +1,16 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_unival_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        fn is_val(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> bool {
+            if let Some(x) = root {
+                x.borrow().val == val && is_val(x.borrow().left.clone(), val)
+                    && is_val(x.borrow().right.clone(), val)
+            } else {
+                true
+            }
+        }
+        let val = root.clone().unwrap().borrow().val;
+        is_val(root, val)
+    }
+}
