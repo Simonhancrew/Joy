5797d89ea0628981f96814ab93fbe5acc2645b7e
simonhancrew <597494370@qq.com>
2022-07-07 13:02:03 +0800

    trie tree

---
+++ LeetCode/DataStruct/CPP/Leetcode648-replace-word.h
@@ -0,0 +1,58 @@
+#include <string>
+
+using namespace std;
+
+class trie {
+public:
+  trie() : flag(false) {
+    for (int i = 0; i < 26; i++)
+      son[i] = nullptr;
+  }
+  void insert(string s) {
+    auto node = this;
+    for (int i = 0; i < s.size(); i++) {
+      int id = s[i] - 'a';
+      if (!node->son[id])
+        node->son[id] = new trie();
+      node = node->son[id];
+    }
+    node->flag = true;
+  }
+
+  int query(string s) {
+    auto node = this;
+    for (int i = 0; i < s.size(); i++) {
+      int id = s[i] - 'a';
+      if (!node->son[id])
+        return s.size();
+      node = node->son[id];
+      if (node->flag)
+        return i + 1;
+    }
+    return s.size();
+  }
+  trie *son[26];
+  bool flag;
+};
+
+class Solution {
+public:
+  trie *root;
+  string replaceWords(vector<string> &dictionary, string sentence) {
+    root = new trie();
+    for (auto s : dictionary)
+      root->insert(s);
+    string res;
+    for (int i = 0; i < sentence.size(); i++) {
+      int k = i;
+      while (k < sentence.size() && sentence[k] != ' ')
+        k++;
+      string t = sentence.substr(i, k - i);
+      int idx = root->query(t);
+      res += t.substr(0, idx) + ' ';
+      i = k;
+    }
+    res.pop_back();
+    return res;
+  }
+};
---
+++ LeetCode/DataStruct/Python/Leetcode648-wordReplace.py
@@ -0,0 +1,29 @@
+class trie:
+    def __init__(self):
+        self.son = defaultdict(trie)
+        self.flag = False
+
+    def insert(self,s):
+        node = self
+        for ch in s:
+            if not node.son[ch]: node.son[ch] = trie()
+            node = node.son[ch]
+        node.flag = True
+     
+    def query(self,s):
+        node = self
+        for i,ch in enumerate(s):
+            if not node.son[ch]: return s
+            node = node.son[ch]
+            if node.flag: return s[:i + 1]
+        return s
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        root = trie()
+        for word in dictionary:
+            root.insert(word)
+        res = []
+        for word in sentence.split(' '):
+            res.append(root.query(word))
+        return ' '.join(res)
