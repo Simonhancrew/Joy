a941e879f6b59d90bea8e2530193581df1f13716
simonhancrew <597494370@qq.com>
2022-08-06 16:23:05 +0800

    simulation

---
+++ LeetCode/Search/CPP/Leetcode1408-string-matching-in-an-array/submissions.h
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    vector<string> stringMatching(vector<string> &words)
+    {
+        vector<string> res;
+        int n = words.size();
+        sort(words.begin(), words.end(), [](auto lhs, auto rhs)
+             { return lhs.size() < rhs.size(); });
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (check(words[i], words[j]))
+                {
+                    res.emplace_back(std::move(words[i]));
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+    /*
+        try kmp later
+    */
+    bool check(string &a, string &b)
+    {
+        int n = a.size();
+        for (int i = 0; i <= b.size() - n; i++)
+        {
+            if (a[0] == b[i] && a == b.substr(i, n))
+                return true;
+        }
+        return false;
+    }
+};
