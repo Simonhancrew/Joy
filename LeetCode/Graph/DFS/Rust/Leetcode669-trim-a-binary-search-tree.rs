56798db536971cdf62bf8931cb5e717af07ec08f
simonhancrew <597494370@qq.com>
2022-09-10 20:17:06 +0800

    BST's DFS

---
+++ LeetCode/Graph/DFS/Rust/Leetcode669-trim-a-binary-search-tree.rs
@@ -0,0 +1,25 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn trim_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(r) = root {
+            if r.borrow().val > high {
+                return Self::trim_bst(r.borrow_mut().left.take(), low, high);
+            }
+
+            if r.borrow().val < low {
+                return Self::trim_bst(r.borrow_mut().right.take(), low, high);
+            }
+
+            let left = Self::trim_bst(r.borrow_mut().left.take(), low, high);
+            let right = Self::trim_bst(r.borrow_mut().right.take(), low, high);
+
+            r.borrow_mut().left = left;
+            r.borrow_mut().right = right;
+
+            Some(r)
+        } else {
+            None
+        }
+    }
+}
