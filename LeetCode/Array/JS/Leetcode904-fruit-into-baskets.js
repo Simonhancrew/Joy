8f6bbfa9de228d4d74e81fb2333041eae4f0940a
simonhancrew <597494370@qq.com>
2022-10-17 23:13:35 +0800

    format code

--- LeetCode/Array/JS/Leetcode904-fruit-into-baskets.js
+++ LeetCode/Array/JS/Leetcode904-fruit-into-baskets.js
@@ -1,12 +1,12 @@
-var totalFruit = function(fruits) {
+var totalFruit = function (fruits) {
   let mp = new Map();
-  let [res,n] = [0, fruits.length];
-  for(let [i,j,s] = [0,0,0];i < n;i++) {
+  let [res, n] = [0, fruits.length];
+  for (let [i, j, s] = [0, 0, 0]; i < n; i++) {
     mp.set(fruits[i], (mp.get(fruits[i]) | 0) + 1);
-    if(mp.get(fruits[i]) == 1) s++;
-    while(s > 2) {
+    if (mp.get(fruits[i]) == 1) s++;
+    while (s > 2) {
       mp.set(fruits[j], mp.get(fruits[j]) - 1);
-      if(mp.get(fruits[j]) == 0) s--;
+      if (mp.get(fruits[j]) == 0) s--;
       j++;
     }
     res = Math.max(res, i - j + 1);
