821280ad146581313dbaedfb5339ac68cf0c6da5
simonhan <597494370@qq.com>
2022-12-16 21:44:20 +0800

    上取整

---
+++ LeetCode/Math/Rust/Leetcode1785-minimum-elements-to-add-to-form-a-given-sum.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn min_elements(nums: Vec<i32>, limit: i32, goal: i32) -> i32 {
+        let acc:i64 = nums.iter().map(|&x| x as i64).sum();
+        let (goal, limit) = (goal as i64, limit as i64);
+        if acc == goal {
+            return 0;
+        }
+        let mut diff = (goal.max(acc) - goal.min(acc)).abs();
+        // print!("acc: {}, goal: {}, diff: {}", acc, goal, diff);
+        let  res = (diff + limit - 1) / limit;
+        res as i32
+    }
+}
