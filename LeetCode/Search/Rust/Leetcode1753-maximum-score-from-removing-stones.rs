699d23e4bea7bb4ab53dcf3b66d0ce071c2d17dc
simonhancrew <597494370@qq.com>
2022-12-21 12:28:13 +0800

    greedy

---
+++ LeetCode/Search/Rust/Leetcode1753-maximum-score-from-removing-stones.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn maximum_score(a: i32, b: i32, c: i32) -> i32 {
+        let sum = a + b + c;
+        let mx = a.max(b).max(c);
+        if sum - mx < mx {
+            return sum - mx;
+        }
+        sum / 2
+    }
+}
