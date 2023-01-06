b940e9ca5269d88eb61025382a5b1f0e86ca2bb9
simonhancrew <597494370@qq.com>
2023-01-06 20:11:10 +0800

    Math

---
+++ LeetCode/Math/Rust/Leetcode2180-count-integers-with-even-digit-sum.rs
@@ -0,0 +1,16 @@
+impl Solution {
+  pub fn count_even(num: i32) -> i32 {
+      let (mut base, last) = (num / 10, num % 10);
+      let (mut res, mut acc) = (base * 5, 0);
+      while base != 0 {
+          acc += base % 10;
+          base /= 10;
+      }
+      if acc & 1 == 1 {
+          res += (last + 1) / 2;
+      } else {
+          res += last / 2 + 1;
+      }
+      res - 1
+  }
+}
