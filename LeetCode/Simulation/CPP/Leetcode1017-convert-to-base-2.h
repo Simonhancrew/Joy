1b840d063a31311c6cffd34c3b172a6983dc559f
simon <597494370@qq.com>
2024-04-28 21:52:55 +0800

    [LC] 1017: mod + div

--- LeetCode/Simulation/CPP/Leetcode1017-convert-to-base-2.h
+++ LeetCode/Simulation/CPP/Leetcode1017-convert-to-base-2.h
@@ -3,30 +3,25 @@
 
 using namespace std;
 
+// n = a[k] * base^k + a[k-1] * base^(k-1) + ... + a[1] * base + a[0]
+// n % base = a[0] * (flag)
+// n / base = a[k] * base^(k-1) + a[k-1] * base^(k-2) + ... + a[1]
+// 做进制转换
+
 class Solution {
 public:
-    string baseNeg2(int n) {
-        if (!n) return "0";
-        string res;
-        while (n) {
-            res.push_back('0' + n % 2);
-            n /= 2;
-        }
-        n = res.size();
-        int cnt = 0;
-        for (int i = 0;i < n;i++) {
-            int cur = cnt + (res[i] - '0');
-            res[i] = '0' + cur % 2;
-            cnt = cur / 2;
-            if (i & 1 && cur & 1) cnt++;
-        }
-        while (cnt) {
-            res.push_back('1');
-            if (n % 2 == 0) cnt--; 
-            n++;
-        }
-        reverse(res.begin(), res.end());
-        return res;
+  string baseNeg2(int n) {
+    if (!n) {
+      return "0";
     }
+    string res;
+    int base = -2;
+    while (n != 0) {
+      int cur = abs(n % base);
+      res.push_back('0' + cur);
+      n = (n - cur) / base;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+  }
 };
-
