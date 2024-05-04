116644869efe6741a3fc82b2f57b46afa0145e2f
simon <597494370@qq.com>
2024-05-04 15:40:45 +0800

    [LC] 1235: dp + 2search

--- LeetCode/DP/CPP/Leecode1235-maximum-profit-in-job-scheduling.h
+++ LeetCode/DP/CPP/Leecode1235-maximum-profit-in-job-scheduling.h
@@ -1,43 +1,36 @@
-#include <vector>
 #include <algorithm>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
-// 线性dp + 二分。
-// 首先按照结束时间将整体排序,f[i]表示前i个item的最大利润
-// 针对i，有选和不选两种可能，分别讨论，选的时候，只要前面的item的右边界
-// 是小于当前i的左边界的就可以选择。这里可以用二分找到之前右边界小于等于i左边界的j
-// f[i] = max(f[i], f[j] + arr[i].profit),
-// 其中i初始化f[i] = max(f[i - 1], arr[i].profit)
+// 因为要不交叠，天然想到按照终点排序然后遍历每个点
+// 针对每一个点，选/不选，选的话，前面一个要不交叠
+// 所以前面能选的点要满足endTime[j] <= startTime[i],
+// i是要选的点，j是前面能选的最大的点，这个二分就可以
+// 因此满足一个递推的步骤，f[i] = max(f[i-1], f[j] + profit[i])
+
 class Solution {
- public:
-  struct Item {
-    int l, r, p;
-    Item(int l, int r, int p) : l(l), r(r), p(p) {}
-  };
-  int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-    const int n = startTime.size();
-    vector<Item> arr;
-    for (int i = 0; i < startTime.size(); i++) {
-      arr.emplace_back(startTime[i], endTime[i], profit[i]);
-    }
-    sort(arr.begin(), arr.end(), [](const auto& lhs, const auto& rhs) { return lhs.r < rhs.r; });
-    vector<int> f(n);
-    f[0] = arr[0].p;
-    for (int i = 1; i < n; i++) {
-      f[i] = max(f[i - 1], arr[i].p);
-      if (arr[0].r <= arr[i].l) {
-        int l = 0, r = i - 1;
-        while (l < r) {
-          int mid = l + r + 1 >> 1;
-          if (arr[mid].r <= arr[i].l)
-            l = mid;
-          else
-            r = mid - 1;
-        }
-        f[i] = max(f[i], f[r] + arr[i].p);
+public:
+  int jobScheduling(vector<int> &startTime, vector<int> &endTime,
+                    vector<int> &profit) {
+    int n = startTime.size();
+    vector<int> arr(n);
+    iota(arr.begin(), arr.end(), 0);
+    ranges::sort(arr, [&](int l, int r) { return endTime[l] < endTime[r]; });
+    vector<int> f(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+      int l = -1, r = i + 1;
+      while (l + 1 < r) {
+        auto mid = (l + r) >> 1;
+        if (endTime[arr[mid]] <= startTime[arr[i]])
+          l = mid;
+        else
+          r = mid;
       }
+      auto j = l;
+      f[i + 1] = max(f[i], f[j + 1] + profit[arr[i]]);
     }
-    return f[n - 1];
+    return f[n];
   }
 };
