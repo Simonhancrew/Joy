cb95e5de1059059f34ea7f6b1fef655256625ade
simon <597494370@qq.com>
2024-06-16 14:44:13 +0800

    [LC] 522: LCS

--- LeetCode/Array/CPP/Leetcode522-longest-uncommon-subsequence-ii.h
+++ LeetCode/Array/CPP/Leetcode522-longest-uncommon-subsequence-ii.h
@@ -1,24 +1,47 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Solution {
- public:
-  bool check(const string &a, const string &b) {
-    int j = 0, n = b.size(), m = a.size();
-    for (int i = 0; i < n; i++) {
-      if (j < m && a[j] == b[i]) j++;
-    }
-    return j == m;
+public:
+  static int Lcs(const std::string &a, const std::string &b) {
+    vector<vector<int>> f(a.size(), vector<int>(b.size(), -1));
+    auto dfs = [&a, &b, &f](auto &&self, int i, int j) {
+      if (i == a.size() || j == b.size()) {
+        return 0;
+      }
+      auto &res = f[i][j];
+      if (res != -1) {
+        return res;
+      }
+      if (a[i] == b[j]) {
+        return res = 1 + self(self, i + 1, j + 1);
+      }
+      return res = max(self(self, i + 1, j), self(self, i, j + 1));
+    };
+    return dfs(dfs, 0, 0);
   }
+
   int findLUSlength(vector<string> &strs) {
-    int n = strs.size(), res = -1;
+    int n = strs.size();
+    ranges::sort(strs, [](auto &l, auto &r) { return l.size() > r.size(); });
     for (int i = 0; i < n; i++) {
-      bool flag = false;
+      int sz = strs[i].size();
+      bool ok = true;
       for (int j = 0; j < n; j++) {
-        if (i != j && check(strs[i], strs[j])) {
-          flag = true;
+        if (i == j)
+          continue;
+        if (Lcs(strs[i], strs[j]) == strs[i].size()) {
+          ok = false;
           break;
         }
       }
-      if (!flag) res = max(res, (int)strs[i].size());
+      if (ok) {
+        return strs[i].size();
+      }
     }
-    return res;
+    return -1;
   }
 };
