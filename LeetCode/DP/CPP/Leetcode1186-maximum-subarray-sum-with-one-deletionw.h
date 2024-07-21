c0c95f2baa464efe9e2fd1c94b0794f2987432e8
simon <597494370@qq.com>
2024-07-21 13:17:13 +0800

    [LC] 1186: add dfs

--- LeetCode/DP/CPP/Leetcode1186-maximum-subarray-sum-with-one-deletionw.h
+++ LeetCode/DP/CPP/Leetcode1186-maximum-subarray-sum-with-one-deletionw.h
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Solution {
- public:
+public:
   int maximumSum(vector<int> &arr) {
     const int n = arr.size();
     vector<int> f(n), g(n);
@@ -17,3 +17,35 @@ class Solution {
     return ans;
   }
 };
+
+#if dfs
+class Solution {
+public:
+  int maximumSum(vector<int> &arr) {
+    int n = arr.size();
+    vector<vector<int>> f(n, vector<int>(2, -1));
+    int ans = INT_MIN;
+    auto dfs = [&arr, &f, &ans](auto &&dfs, int i, int can) {
+      int &res = f[i][can];
+      if (res != -1) {
+        return res;
+      }
+      if (i == 0) {
+        res = arr[0];
+        ans = max(ans, res);
+        return res;
+      }
+      res = arr[i];
+      auto cur = dfs(dfs, i - 1, can) + arr[i];
+      if (can) {
+        cur = max(cur, dfs(dfs, i - 1, 0));
+      }
+      res = max(res, cur);
+      ans = max(ans, res);
+      return res;
+    };
+    dfs(dfs, n - 1, 1);
+    return ans;
+  }
+};
+#endif
