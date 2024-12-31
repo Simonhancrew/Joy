0bc9c44700999c0c1f016648a62f66223fccdedd
simon <597494370@qq.com>
2024-12-31 20:03:36 +0800

    [LC] 3219: greedy

---
+++ LeetCode/Greedy/CPP/Leetcode3219-minimum-cost-for-cutting-cake-ii.h
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  using LL = long long;
+  LL minimumCost(int m, int n, vector<int> &horizontalCut,
+                 vector<int> &verticalCut) {
+    ranges::sort(horizontalCut, [](auto &lhs, auto &rhs) {
+      return lhs > rhs;
+    });
+    ranges::sort(verticalCut, [](auto &lhs, auto &rhs) {
+      return lhs > rhs;
+    });
+    int i = 0, j = 0;
+    int h = 1, v = 1;
+    LL ans = 0;
+    while (i < horizontalCut.size() || j < verticalCut.size()) {
+      if (j == verticalCut.size() ||
+          (i < horizontalCut.size() && horizontalCut[i] > verticalCut[j])) {
+        ans += horizontalCut[i] * v;
+        h++;
+        i++;
+      } else {
+        ans += verticalCut[j] * h;
+        v++;
+        j++;
+      }
+    }
+    return ans;
+  }
+};
