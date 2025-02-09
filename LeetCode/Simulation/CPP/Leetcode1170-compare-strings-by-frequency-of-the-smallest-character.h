56c37beb4d7a6833855b26a1035d6cde0ab80ce8
simon <597494370@qq.com>
2025-02-09 16:06:12 +0800

    [LC] 1170: update binary search

--- LeetCode/Simulation/CPP/Leetcode1170-compare-strings-by-frequency-of-the-smallest-character.h
+++ LeetCode/Simulation/CPP/Leetcode1170-compare-strings-by-frequency-of-the-smallest-character.h
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+#if 0
 class Solution {
  public:
   vector<int> numSmallerByFrequency(vector<string>& queries,
@@ -32,6 +33,55 @@ class Solution {
     };
     for (int i = 0; i < n; i++) {
       ans[i] = check(i);
+    }
+    return ans;
+  }
+};
+
+#endif
+
+// 2025/02/09
+
+class Solution {
+ public:
+  static int get(const std::string &s) {
+    int res = 0, cur = 'z';
+    for (char i : s) {
+      if (i < cur) {
+        res = 0;
+        cur = i;
+      }
+      if (i == cur) {
+        ++res;
+      }
+    }
+    return res;
+  }
+  vector<int> numSmallerByFrequency(vector<string> &queries,
+                                    vector<string> &words) {
+    vector<int> fq, fw;
+    fq.reserve(queries.size());
+    fw.reserve(fw.size());
+    for (const auto &q : queries) {
+      fq.push_back(get(q));
+    }
+    for (const auto &w : words) {
+      fw.push_back(get(w));
+    }
+    ranges::sort(fw);
+    vector<int> ans;
+    ans.reserve(fq.size());
+    int n = fw.size();
+    for (const auto x : fq) {
+      int l = -1, r = n;
+      while (l + 1 < r) {
+        int mid = l + r >> 1;
+        if (fw[mid] <= x)
+          l = mid;
+        else
+          r = mid;
+      }
+      ans.push_back(n - r);
     }
     return ans;
   }
