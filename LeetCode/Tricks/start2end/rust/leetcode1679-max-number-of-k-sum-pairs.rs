348f083522331337231a6b6edfe3aab84c2bfb3d
yourfather <597494370@qq.com>
2025-05-25 11:41:53 +0800

    [LC] 1679: iter right, check left

---
+++ LeetCode/Tricks/start2end/rust/leetcode1679-max-number-of-k-sum-pairs.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+// iter right, check left
+impl Solution {
+    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut mp = HashMap::new();
+        let mut ans = 0;
+        for x in nums {
+            if x >= k {
+                continue;
+            }
+            if let Some(v) = mp.get_mut(&x) {
+                *v -= 1;
+                ans += 1;
+                if *v == 0 {
+                    mp.remove(&x);
+                }
+            } else {
+                mp.entry(k - x).and_modify(|v| *v += 1).or_insert(1);
+            }
+        }
+        ans
+    }
+}
